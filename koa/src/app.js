88c787e9fabbb445fc41e5fe91611ddffe412130
Daniel <satelerd@gmail.com>
2022-11-28 12:19:00 -0300

    inital config

--- koa/src/app.js
+++ koa/src/app.js
@@ -1,1 +1,18 @@
-console.log("Hello World");
+import Koa from 'koa';
+import KoaBody from 'koa-body';
+import KoaLogger from 'koa-logger';
+import router from './routes.js';
+
+const app = new Koa();
+
+// app.use(KoaBody());
+app.use(KoaLogger());
+
+app.use((ctx, next) => {
+    ctx.body = 'Hello World';
+});
+
+app.listen(3000, () => {
+    console.log('Server is running on port 3000');
+}
+);
